7c204b433be2cc871971d1fb7e1c47ba4a739b99
Takeru Kakuta <kakuta19960915@gmail.com>
2024-04-29 15:44:50 +0900

    💄 headerのレイアウト追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <header className='text-gray-700 border-b border-gray-200'>
+      <div className='container flex mx-auto p-5 flex-col md:flex-row items-center'>
+        <a href="#" className='font-medium text-gray-900 mb-4 md:mb-0'>
+          <span className='text-xl'>Kakuta Blog</span>
+        </a>  
+        <nav className='md:ml-auto text-base'>
+          <a href="#home" className='mr-5 hover:text-blue-400 duration-300'>Home</a>
+          <a href="#about" className='mr-5 hover:text-blue-400 duration-300' >About</a>
+          <a href="#skills" className='mr-5 hover:text-blue-400 duration-300'>Skills</a>
+          <a href="#blog" className=' hover:text-blue-400 duration-300'>Blog</a>
+        </nav>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </header>
+      )
 }
 
 export default App
